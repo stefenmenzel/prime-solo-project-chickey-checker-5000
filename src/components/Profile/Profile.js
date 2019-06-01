7dd2ea6d88d024edd4f9a8401f243abc1db6a63e
Stefen Menzel <stuff@Stefens-MacBook-Pro.local>
2019-06-01 12:34:20 -0500

    got the profile page to update properly after a form change is submitted

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -8,17 +8,35 @@ class Profile extends Component{
         first_name: this.props.user.first_name,
         last_name: this.props.user.last_name,
         email: this.props.user.email,
-        phone_number: this.props.user.phone_number
+        phone_number: this.props.user.phone_number,
+        isClicked: false,
     }
 
     componentDidMount(){
         console.log('this.state on mount:', this.state);
+        this.setState({
+            first_name: this.props.user.first_name,
+            last_name: this.props.user.last_name,
+            email: this.props.user.email,
+            phone_number: this.props.user.phone_number, 
+        })
     }
 
     handleSubmit = (event) => {
         event.preventDefault();
-        console.log('saving profile');        
-        this.props.dispatch({type: 'UPDATE_PROFILE', payload: this.state});        
+        console.log('saving profile');
+        this.props.dispatch({type: 'UPDATE_PROFILE', payload: this.state});
+        this.setState({
+            ...this.state,
+            isClicked: false,
+        })
+    }
+
+    toggleInput = () => {
+        this.setState({
+            ...this.state,
+            isClicked: true,
+        })
     }
 
     handleChange = (propertyToChange, event) => {
@@ -35,10 +53,10 @@ class Profile extends Component{
             <div>
                 <h1>Profile</h1>
                 <form onSubmit={this.handleSubmit}>
-                    <div>First Name: <ProfileItem handleChange={this.handleChange} valueToChange="first_name" item={this.state.first_name} /></div>
-                    <div>Last Name: <ProfileItem handleChange={this.handleChange} valueToChange="last_name" item={this.state.last_name} /></div>                    
-                    <div>Email: <ProfileItem handleChange={this.handleChange} valueToChange="email" item={this.state.email} /></div>
-                    <div>Phone Number: <ProfileItem handleChange={this.handleChange} valueToChange="phone_number" item={this.state.phone_number} /></div>
+                    <div>First Name: <ProfileItem toggleInput={this.toggleInput} isClicked={this.state.isClicked} handleChange={this.handleChange} valueToChange="first_name" item={this.state.first_name} /></div>
+                    <div>Last Name: <ProfileItem toggleInput={this.toggleInput} isClicked={this.state.isClicked} handleChange={this.handleChange} valueToChange="last_name" item={this.state.last_name} /></div>                    
+                    <div>Email: <ProfileItem toggleInput={this.toggleInput} isClicked={this.state.isClicked} handleChange={this.handleChange} valueToChange="email" item={this.state.email} /></div>
+                    <div>Phone Number: <ProfileItem toggleInput={this.toggleInput} isClicked={this.state.isClicked} handleChange={this.handleChange} valueToChange="phone_number" item={this.state.phone_number} /></div>
                     <button type="submit">save</button>
                 </form>
             </div>
--- src/components/Profile/ProfileItem.js
+++ src/components/Profile/ProfileItem.js
@@ -1,25 +1,31 @@
 import React, {Component} from 'react';
 
 class ProfileItem extends Component{
-    state = {
-        isClicked: false,        
-    }
+    // state = {
+    //     isClicked: false,        
+    // }
 
-    toggleInput = () => {
-        this.setState({
-            ...this.state,
-            isClicked: !this.state.isClicked
-        })
-    }
+    // toggleInput = () => {
+    //     this.setState({
+    //         ...this.state,
+    //         isClicked: !this.state.isClicked
+    //     })
+    // }
 
     render(){
         return(
-            (this.state.isClicked) ?            
+            (this.props.isClicked) ?            
                 <input placeholder={this.props.item} onChange={(e) => { this.props.handleChange(this.props.valueToChange, e) }} />            
             :
-            <span onClick={this.toggleInput}>{(this.props.item) ? this.props.item: '*EMPTY*'}</span>
+            <span onClick={this.props.toggleInput}>{(this.props.item) ? this.props.item: '*EMPTY*'}</span>
         )
     }
+}
+
+const mapStateToProps = (reduxState) => {
+    return {
+        user: reduxState.user
+    };
 }
 
 export default ProfileItem;
